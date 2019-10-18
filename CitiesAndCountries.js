3444702d0543d0662b159a5e5923390dce56c398
Vardan GHULYAN <vghulyan@yahoo.com>
2019-10-19 00:44:15 +0100

    The MainApp

---
+++ CitiesAndCountries.js
@@ -0,0 +1,83 @@
+const fetch = require('node-fetch');
+var fs = require("fs");
+const isoCountries = require('./MainApp');
+
+//const isoCodes = ['AF', 'AX', 'AL', 'DZ', 'AS', 'AD', 'AO', 'AI', 'AQ', 'AG', 'AR', 'AM', 'AW', 'AU', 'AT', 'AZ', 'BH', 'BS', 'BD', 'BB', 'BY', 'BE', 'BZ', 'BJ', 'BM', 'BT', 'BO', 'BQ', 'BA', 'BW', 'BV', 'BR', 'IO', 'BN', 'BG', 'BF', 'BI', 'KH', 'CM', 'CA', 'CV', 'KY', 'CF', 'TD', 'CL', 'CN', 'CX', 'CC', 'CO', 'KM', 'CG', 'CD', 'CK', 'CR', 'CI', 'HR', 'CU', 'CW', 'CY', 'CZ', 'DK', 'DJ', 'DM', 'DO', 'EC', 'EG', 'SV', 'GQ', 'ER', 'EE', 'ET', 'FK', 'FO', 'FJ', 'FI', 'FR', 'GF', 'PF', 'TF', 'GA', 'GM', 'GE', 'DE', 'GH', 'GI', 'GR', 'GL', 'GD', 'GP', 'GU', 'GT', 'GG', 'GN', 'GW', 'GY', 'HT', 'HM', 'VA', 'HN', 'HK', 'HU', 'IS', 'IN', 'ID', 'IR', 'IQ', 'IE', 'IM', 'IL', 'IT', 'JM', 'JP', 'JE', 'JO', 'KZ', 'KE', 'KI', 'KP', 'KR', 'KW', 'KG', 'LA', 'LV', 'LB', 'LS', 'LR', 'LY', 'LI', 'LT', 'LU', 'MO', 'MK', 'MG', 'MW', 'MY', 'MV', 'ML', 'MT', 'MH', 'MQ', 'MR', 'MU', 'YT', 'MX', 'FM', 'MD', 'MC', 'MN', 'ME', 'MS', 'MA', 'MZ', 'MM', 'NA', 'NR', 'NP', 'NL', 'NC', 'NZ', 'NI', 'NE', 'NG', 'NU', 'NF', 'MP', 'NO', 'OM', 'PK', 'PW', 'PS', 'PA', 'PG', 'PY', 'PE', 'PH', 'PN', 'PL', 'PT', 'PR', 'QA', 'RE', 'RO', 'RU', 'RW', 'BL', 'SH', 'KN', 'LC', 'MF', 'PM', 'VC', 'WS', 'SM', 'ST', 'SA', 'SN', 'RS', 'SC', 'SL', 'SG', 'SX', 'SK', 'SI', 'SB', 'SO', 'ZA', 'GS', 'SS', 'ES', 'LK', 'SD', 'SR', 'SJ', 'SZ', 'SE', 'CH', 'SY', 'TW', 'TJ', 'TZ', 'TH', 'TL', 'TG', 'TK', 'TO', 'TT', 'TN', 'TR', 'TM', 'TC', 'TV', 'UG', 'UA', 'AE', 'GB', 'US', 'UM', 'UY', 'UZ', 'VU', 'VE', 'VN', 'VG', 'VI', 'WF', 'EH', 'YE', 'ZM', 'ZW'];
+const isoCodes = ['AM', 'RU'];
+console.log('Extracting ' + isoCodes.length + ' records...');
+
+const allCountries = [];
+const country = [];
+const url = 'http://api.geonames.org/searchJSON?username=ksuhiyp&maxRows=1000&country=';
+//let writer = fs.createWriteStream('./cities2.json');
+const writerStream = fs.createWriteStream('./cities2.json', {flags: 'a'})
+    .on('finish', function() {
+        console.log("Write Finish.");
+    })
+    .on('error', function(err){
+        console.log(err.stack);
+    });
+
+async function wait(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+}
+
+const fetchPublicReposCity = async (isoCode, index) => {
+    const start = new Date();
+
+    const response = await fetch(`${url}${isoCode}`);
+    const json = await response.json();
+
+    const end = new Date();
+    console.log(index, ' - Extracted: ', isoCode, ' in: ', (end - start), ' miliseconds');
+    return json;
+};
+
+const fetchAllIsoCodes = async () => {
+    const promises = isoCodes.map(async (isoCode, index) => {
+        const response = await fetchPublicReposCity(isoCode, index);
+        const { geonames } = response;
+        geonames.map(g => {
+            console.log('COUNTRY NAME: ', g.countryName);
+            country.push({
+                countryName: g.countryName,
+                name: g.name,
+                countryCode: g.countryCode,
+                lng: g.lng,
+                lat: g.lat
+            })
+        });
+
+        //allCountries.push(country);
+console.log('waiting 1000');
+        await wait(1000);
+        return country;
+        //return response;
+        //return allCountries;
+    });
+    return Promise.all(promises);
+};
+
+const main = async () => {
+    const start = new Date();
+
+    //await fetchAllIsoCodes();
+    const response = await fetchAllIsoCodes();
+    writerStream.write(JSON.stringify(response), () => {
+        console.log('Data successfully been written.........');
+    }); // (response, null, 4);
+
+    const end = new Date();
+    console.log('Time taken to complete the task is: ', end - start);
+
+    allCountries.map(al => {
+        console.log(al);
+    });
+
+    writerStream.end();
+};
+
+main();
+
